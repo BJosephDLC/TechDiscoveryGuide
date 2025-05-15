f7346de3410d15fba137c7317fd9bc33ff0aab96
BJosephDLC <BernardoJDeLaCruz@gmail.com>
2025-05-15 13:49:46 -0700

    Set up initial project structure with backend and frontend

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "frontend/build")));
 
 // Root route serving the landing page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.listen(PORT, () => {
